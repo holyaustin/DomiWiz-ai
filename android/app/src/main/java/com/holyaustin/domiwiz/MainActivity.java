2e23562a83337949a3ea8e93780931c242c5485c
holyaustin <holyaustin@yahoo.com>
2025-01-31 22:48:58 +0100

    first commit

---
+++ android/app/src/main/java/com/holyaustin/domiwiz/MainActivity.java
@@ -0,0 +1,5 @@
+package com.holyaustin.domiwiz;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.holyaustin.domiwiz',
+  appName: 'domiwiz',
+  webDir: 'dist'
+};
+
+export default config;
